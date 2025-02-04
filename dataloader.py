f41553abf2c7bbedd76a2950f12f37d7be21d036
artrela <trelaalec@gmail.com>
2025-02-03 22:02:49 -0500

    Changes: add ray caster

--- dataloader.py
+++ dataloader.py
@@ -5,9 +5,8 @@ import os
 import sys
 import torch
 
-class SyntheticDataloader(torch.utils.data.Dataset):
+class SyntheticDataloader():
     def __init__(self, pth, item, split="train"):
-        super().__init__()
         
         data_pth = os.path.join(pth + "/" + item)
         
@@ -22,18 +21,38 @@ class SyntheticDataloader(torch.utils.data.Dataset):
         
         self.total_pxs = self.images[0].shape[0] * self.images[0].shape[1]
         self.img_shape = self.images[0].shape[:2]
+        self.img_shape = (800, 800)
         
-        f = (self.img_shape[0] / 2) / np.tan( data['camera_angle_x'] / 2 ) 
+        self.f = (self.img_shape[0] / 2) / np.tan( data['camera_angle_x'] / 2 ) 
+        self.cx = -self.img_shape[0] / 2
+        self.cy =  self.img_shape[0] / 2
         self.cam2img = np.eye(3)
-        self.cam2img[0, 0] = self.cam2img[1, 1] = f
-        self.cam2img[0, 2] = self.cam2img[1, 2] = self.img_shape[0] / 2
+        self.cam2img[0, 0] = self.cam2img[1, 1] = self.f
+        self.cam2img[0, 2] = self.cx
+        self.cam2img[1, 2] =  self.cy
+    
+    def create_rays(self, R, t):
+        '''
+        R.shape = 3x3
+        t.shape = 1x3
+        '''
         
-    def __getitem__(self, idx):
+        # TODO - do this for a SET of Rotation Matric
+        u, v = np.mgrid[0:self.img_shape[0], 0:self.img_shape[1]]
         
-        random_px = np.random.randint(0, self.total_pxs)
-        px_pos = np.unravel_index(random_px, self.img_shape)
+        xc = (u - self.cx) / self.f
+        yc = (v - self.cy) / self.f
         
-        return px_pos
+        # TODO change back to 1 afterwards!
+        # d = np.dstack((xc, yc, np.ones(xc.shape))) @ R
+        d = np.dstack((xc, yc, -np.ones(xc.shape))) @ R
+        o = np.ones((*self.img_shape, 1)) @ t
+        
+        return o, d
+    
+    def sample_ray(o, d, t):
+        pass
+        
     
     def __len__(self):
         assert len(self.transforms) == len(self.images)
